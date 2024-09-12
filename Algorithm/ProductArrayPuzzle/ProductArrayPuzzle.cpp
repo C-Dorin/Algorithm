34be94d586b9d00f59b459dbfe479e45f50b3a35
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2024-09-12 20:02:18 +0300

    GeeksForGeeks - productExceptSelf (Easy mode)

---
+++ Algorithm/ProductArrayPuzzle/ProductArrayPuzzle.cpp
@@ -0,0 +1,46 @@
+//
+//  ProductArrayPuzzle.cpp
+//  Algorithm
+//
+//  Created by Dorin Curnic on 11.09.2024.
+//
+
+#include "ProductArrayPuzzle.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<long long int> productExceptSelf(){
+    vector<long long int> nums = {10, 3, 5, 6, 2};
+    long long int product = 1;
+    int c = 0;
+    
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            product *= nums[i];
+        } else {
+            c++;
+        }
+    }
+    
+    if (c > 1) {
+        for (int i = 0; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+    } else if (c == 1){
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                nums[i] = product;
+            } else {
+                nums[i] = 0;
+            }
+        }
+    } else {
+        for (int i = 0; i < nums.size(); i++){
+            nums[i] = product/nums[i];
+        }
+    }
+    
+    return nums;
+}
---
+++ Algorithm/ProductArrayPuzzle/ProductArrayPuzzle.hpp
@@ -0,0 +1,18 @@
+//
+//  ProductArrayPuzzle.hpp
+//  Algorithms
+//
+//  Created by Dorin Curnic on 11.09.2024.
+//
+
+#ifndef ProductArrayPuzzle_hpp
+#define ProductArrayPuzzle_hpp
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+vector<long long int> productExceptSelf();
+
+#endif /* ProductArrayPuzzle_hpp */
